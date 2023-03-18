b3d4d156c586a7a5df169c2b3f65758dcef5cf77
karolina269 <120135313+karolina269@users.noreply.github.com>
2023-03-18 20:26:39 +0100

    hamburger menu implementation and forms styling

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("hamburger")[0].addEventListener("click", function () {
+  document.getElementsByClassName("open-menu-wrapper")[0].classList.toggle("open");
+});
+
+document.getElementsByClassName("close")[0].addEventListener("click", function () {
+  document.getElementsByClassName("open-menu-wrapper")[0].classList.toggle("open");
+});
